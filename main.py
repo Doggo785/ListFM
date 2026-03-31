d84a7cd76da549c450dd4587eb88f796ac0012ab
Doggo785 <stephane.plathey@proton.me>
2026-03-31 16:47:45 +0200

    feat(data): add function to retrieve and save recent tracks
    * Implement get_test_data function to fetch recent tracks from Last.fm
    * Write track details to test_data.csv
    * Update .gitignore to include test_data.csv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import pylast
+import csv
 
 load_dotenv()
 
@@ -8,7 +9,26 @@ LASTFM_API_KEY = os.getenv("LASTFM_API_KEY")
 LASTFM_API_SECRET = os.getenv("LASTFM_API_SECRET")
 
 try:
-    lastfm_connection = pylast.LastFMNetwork(api_key=LASTFM_API_KEY, api_secret=LASTFM_API_SECRET)
+    network = pylast.LastFMNetwork(api_key=LASTFM_API_KEY, api_secret=LASTFM_API_SECRET)
     print("Connexion à l'API Last.fm réussie !")
 except Exception as e:
     print(f"Erreur lors de la connexion à l'API Last.fm : {e}")
+
+def get_test_data():
+    try:
+        user = network.get_user("AssaWolf")
+        recent_tracks = user.get_recent_tracks(limit=300)
+        with open('test_data.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Artist', 'Track', 'Album'])
+            for track in recent_tracks:
+                artist = track.track.artist.name
+                track_name = track.track.title
+                album = track.album
+                writer.writerow([artist, track_name, album])
+    except Exception as e:
+        print(f"Erreur lors de la récupération des données : {e}")
+        return []
+
+if __name__ == "__main__":
+    get_test_data()
