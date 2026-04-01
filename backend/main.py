039b45a60b7f89ca8aa6574dc26850b808456153
Doggo <139872771+Doggo785@users.noreply.github.com>
2026-04-01 15:39:56 +0200

    feat: add lastfm communication, extraction and deduplication
    * feat(api): add Last.fm API integration
    
    * Create .env.example for environment variables
    * Implement Last.fm API connection in main.py
    * Load environment variables using dotenv
    
    * feat(data): add function to retrieve and save recent tracks
    
    * Implement get_test_data function to fetch recent tracks from Last.fm
    * Write track details to test_data.csv
    * Update .gitignore to include test_data.csv
    
    * feat(data): add deduplication function for tracks
    
    * Implement deduplication logic to filter unique tracks based on artist and title.
    * Enhance data handling in get_test_data function.
    
    * fix(deduplication): correct identifier key format for tracks
    
    * Change identifier key separator from '-' to '|'
    * Ensures proper deduplication of tracks based on artist and title
    
    * refactor(deduplication): improve track deduplication logic
    
    * Update deduplication function to use object attributes instead of dictionary keys.
    * Enhance readability by renaming loop variable from 'track' to 'item'.
    
    * chore: restructuration

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+import pylast
+import csv
+
+load_dotenv()
+
+LASTFM_API_KEY = os.getenv("LASTFM_API_KEY")
+LASTFM_API_SECRET = os.getenv("LASTFM_API_SECRET")
+
+try:
+    network = pylast.LastFMNetwork(api_key=LASTFM_API_KEY, api_secret=LASTFM_API_SECRET)
+    print("Connexion à l'API Last.fm réussie !")
+except Exception as e:
+    print(f"Erreur lors de la connexion à l'API Last.fm : {e}")
+
+def get_test_data():
+    try:
+        user = network.get_user("AssaWolf")
+        recent_tracks = user.get_recent_tracks(limit=300)
+        with open('test_data.csv', mode='w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Artist', 'Track', 'Album'])
+            for track in recent_tracks:
+                artist = track.track.artist.name
+                track_name = track.track.title
+                album = track.album
+                writer.writerow([artist, track_name, album])
+    except Exception as e:
+        print(f"Erreur lors de la récupération des données : {e}")
+        return []
+
+def deduplicate(raw_tracks: list[dict]) -> list[dict]:
+    seen = set()
+    unique_tracks = []
+    for item in raw_tracks:
+        artist = item.track.artist.name if item.track.artist else 'Unknown Artist'
+        title = item.track.title if item.track.title else 'Unknown Track'
+        identifier_key = f"{artist.strip().lower()}|{title.strip().lower()}"
+        if identifier_key not in seen:
+            seen.add(identifier_key)
+            unique_tracks.append(item)
+    return unique_tracks
+
+if __name__ == "__main__":
+    get_test_data()
